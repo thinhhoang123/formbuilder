8d41c15fe71869a486aae963e407de662d3c9a98
Thinh Gia Hoang <atulala4657@gmail.com>
2026-01-17 20:27:14 +0700

    feat: Editable text

--- src/components/common/form-builder.tsx
+++ src/components/common/form-builder.tsx
@@ -11,8 +11,19 @@ import { Button } from '../ui/button';
 import { TextFieldFormElement } from '../fields/text-field';
 import SidebarBtnElement from '../sidebar-btn-element';
 import Designer from '../designer';
+import { updateFormName } from '@/actions/form';
+import { toast } from 'sonner';
 
 export default function FormBuilder({ form }: { form: Form }) {
+  const handleChangeName = async (newName: string) => {
+    try {
+      await updateFormName(form.id, newName);
+      toast('👍🏻 Update form name successfully');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <ResizablePanelGroup>
       <ResizablePanel defaultSize={40} minSize={300}>
@@ -27,7 +38,9 @@ export default function FormBuilder({ form }: { form: Form }) {
 
       <ResizablePanel defaultSize={100}>
         <div className="border-b w-full p-4 flex items-center justify-between">
-          <Typography variant="h4">{form.name}</Typography>
+          <Typography variant="h4" contentEditable onUpdate={handleChangeName}>
+            {form.name}
+          </Typography>
           <div className="flex gap-2">
             <Button variant="outline">Preview</Button>
             <Button variant="outline">Save</Button>
